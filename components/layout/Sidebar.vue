ab9b0302948770297e1ccd1c6de99332081abc2b
ST-17 <tlepbergen.s@mail.ru>
2024-12-27 00:02:48 +0500

    fix: logo v2

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -18,7 +18,7 @@ const logout = async () => {
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
     <NuxtLink to="/" class="mb-10 block">
-      <NuxtImg src="../../public/logo.svg" width="100px" class="mx-auto" />
+      <NuxtImg src="/logo.svg" width="100px" class="mx-auto" />
     </NuxtLink>
     <button
       class="absolute top-2 right-3 transition-colors hover:text-primary"
